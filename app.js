9774a5c3398dccea8593bc174f789199bb67a513
bindhushree <bindhushreeb05@gmail.com>
2023-11-06 11:16:13 +0530

    completed

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-// Iteration 1: Make the Play button functional.
-// Description: When the Play button is clicked the game page should be displayed.
+//Iteration 1: Make the play button functional.
+//Description: When the play button is clicked the game page should be displayed.
+const play = document.getElementById("play-button");
+play.onclick = () => {
+    window.location.href = "./game.html";
+};
